43b13473dbaa53a665308d520f776157a04d9161
ZhuowenChen <ZhuowenChen>
2016-06-24 14:46:22 -0700

    added bigImage and small class

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-$(document).ready(function() {
-  $("#hide").click(function(){
-    $("#hide").hide();
+$(document).ready(function(){
+  $(".small").click(function(event){
+    event.preventDefault();
+    $(".small").animate({"font-size":"100px"});
+  });
+  $(".bigImage").click(function(event){
+    event.preventDefault();
+    $(".bigImage").animate({"width":"1000px"})
   });
 });
